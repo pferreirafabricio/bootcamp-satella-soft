53fa0226a57b52451448f194de6bebfd9b86c990
Fabricio Pinto Ferreira <pferreirafabricio@gmail.com>
2020-06-13 19:37:49 -0300

    add all form structure and styles

--- exercise-week-4/index.js
+++ exercise-week-4/index.js
@@ -1,1 +1,8 @@
-console.log("hi");
+'use strict'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('registerForm');
+    form.addEventListener('submit', () => {
+        console.log("Submeteu");
+    });
+});
