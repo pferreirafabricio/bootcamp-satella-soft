4eb20e58590348f3c8ff9a6de090d8e81e45f6c1
Fabricio Pinto Ferreira <pferreirafabricio@gmail.com>
2020-06-13 21:49:19 -0300

    refactory: remove redundant code

--- exercise-week-4/index.js
+++ exercise-week-4/index.js
@@ -7,14 +7,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const name = document.forms['registerForm']['txtName'];
     const email = document.forms['registerForm']['txtEmail'];
     const password = document.forms['registerForm']['txtPassword'];
-    const confirmPassowrd = document.forms['registerForm']['txtConfirmPassword'];
+    const confirmPassword = document.forms['registerForm']['txtConfirmPassword'];
     const genre = document.forms['registerForm']['txtGenre'];
     const dateBirth = document.forms['registerForm']['dtDateBirth'];
     const aboutYou = document.forms['registerForm']['txtAboutYou'];
 
     // All Regex
     const nameRegex = /[a-z]{3,50}/;
-    const emailRegex = /[a-z]{5,87}\@[a-z]{3,10}.[a-z]{3}/;
+    const emailRegex = /[a-z]{5,87}\@[a-z]{3,10}.[a-z]{2,3}/;
     const passwordRegex = /\w{7,50}/;
     const genreRegex = /m|f/;
     const dateRegex = /\d{4}\-\d{1,2}\-\d{1,2}/;
@@ -27,14 +27,16 @@ document.addEventListener('DOMContentLoaded', () => {
         valideForm = true;
 
         const formData = {
-            name: document.forms['registerForm']['txtName'].value,
-            email: document.forms['registerForm']['txtEmail'].value,
-            password: document.forms['registerForm']['txtPassword'].value,
-            confirmPassword: document.forms['registerForm']['txtConfirmPassword'].value,
-            genre: document.forms['registerForm']['txtGenre'].value,
-            dateBirth: document.forms['registerForm']['dtDateBirth'].value,
-            aboutYou: document.forms['registerForm']['txtAboutYou'].value,
+            name: name.value,
+            email: email.value,
+            password: password.value,
+            confirmPassword: confirmPassword.value,
+            genre: genre.value,
+            dateBirth: dateBirth.value,
+            aboutYou: aboutYou.value,
         }
+
+        console.log(formData);
 
         if (!validateForm(formData)) {
             event.preventDefault();
@@ -68,13 +70,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
         if (pFormData.confirmPassword !== "") {
             if (pFormData.password !== pFormData.confirmPassword) {
-                confirmPassowrd.classList.add('invalid');
+                confirmPassword.classList.add('invalid');
                 createErrorElement('Both passwords must be the same!');
             } else {
-                confirmPassowrd.classList.add('valid');
+                confirmPassword.classList.add('valid');
             }
         } else {
-            confirmPassowrd.classList.add('invalid');
+            confirmPassword.classList.add('invalid');
             createErrorElement('The "Confirm password" field cannot be empty!!');
         }
 
